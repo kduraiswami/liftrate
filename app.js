a81a673a1b4ba51f3fb62d364beb1da04333044d
Kailash <kaidurai@gmail.com>
2015-06-23 17:20:39 -0700

    created add post function

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('liftRate', []);
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-}]);
+	'$scope',
+	function($scope){
+		$scope.test = 'Hello world!';
+		$scope.workouts = [
+		{title: 'Lower Workout', upvotes: 5},
+		{title: 'Pec Blast', upvotes: 2},
+		{title: 'Full Superset', upvotes: 15},
+		];
+
+		$scope.addPost = function() {
+			$scope.posts.push({title: 'A new post', upvotes: 0})
+		}
+	}
+]);
