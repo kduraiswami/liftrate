5b66250d2894a16207292b6b8ad7249abe978053
Kailash <kaidurai@gmail.com>
2015-06-30 13:56:59 -0700

    set up server requirement orders and imported routes into server file

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
+
+// requirement set up
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+require('dotenv').load();
+
+var port = process.env.PORT || 8080
+
+var database = require('./config/database')
 var morgan = require('morgan');
+
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
-var database = require('./config/database')
-require('dotenv').load();
+
 
 //actually connect to remote db
 mongoose.connect(database.url)
 
+//configure files 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
-app.listen(8080)
-console.log("Application is on 8080")
+//routes too 
+require('./app/routes.js')(app);
+
+app.listen(port)
+
+console.log("Application is on " + port)
